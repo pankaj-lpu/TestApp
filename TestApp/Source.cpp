201f7c3c3d1ddcdd2e3b85359495f6b29164d80b
Pankaj Jagasia <jagasia-lpu@yahoo.com>
2020-02-19 14:17:51 +0530

    Initial Commit

---
+++ TestApp/Source.cpp
@@ -0,0 +1,50 @@
+#include<iostream>
+using namespace std;
+
+class Base
+{
+	int data;
+public:
+	virtual void fn()
+	{
+		cout << "Fn of Base" << endl;
+	}
+
+	virtual void xn()
+	{
+		cout << "Xn of Base" << endl;
+	}
+
+	void mn()
+	{
+		cout << "Mn of Base" << endl;
+	}
+};
+
+class Derived : public Base
+{
+	int dData;
+public : 
+	void fn()
+	{
+		cout << "Fn of Derived ********" << endl;
+	}
+};
+
+
+
+void main()
+{
+	
+	Base* ptr = new Base;
+
+	ptr->fn(); 
+
+	ptr->xn();
+
+	cout << sizeof(*ptr);
+
+	cout << "******************** " << endl;
+
+
+}
